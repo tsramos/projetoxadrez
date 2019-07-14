1b07d14f707cac86c117c1e61af7fa54b4f6de20
Thiago Ramos <thiago.s.ramos@outlook.com>
2019-07-14 13:00:44 -0300

    Criado classe Posicao

---
+++ Xadrez-console/Tabuleiro/Posicao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return $"{Linha},{Coluna}"; 
+        }
+    }
+}
